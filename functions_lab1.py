1d5bc85730e1e2df79b566cb54946fe168d8d6dd
jacobmat8819 <jackmat8819@gmail.com>
2024-07-10 18:48:17 +0530

    version 2.0

--- functions_lab1.py
+++ functions_lab1.py
@@ -8,3 +8,9 @@ def my_name(**name):
     print('My Last name is '+ name['lname'])
 
 my_name1 = my_name(fname = 'Jacob',lname = 'Mathew')
+
+
+def my_function(*, x):
+  print(x)
+
+my_function(x = 3)
