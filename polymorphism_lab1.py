46ea4b6ca54d216c728ba0dcf81f16efb1d9b02a
jacobmat8819 <jackmat8819@gmail.com>
2024-07-11 18:44:02 +0530

    Version 2.0

--- polymorphism_lab1.py
+++ polymorphism_lab1.py
@@ -63,3 +63,61 @@ z = seacreatures('whale','Water')
 
 for i in (x,y,z):
     print(i.properties())
+
+#---------------------------------------------------------
+
+class Vehicle:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Move!")
+
+class Car(Vehicle):
+  pass
+
+class Boat(Vehicle):
+  def move(self):
+    print("Sail!")
+
+class Plane(Vehicle):
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford", "Mustang") #Create a Car object
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+plane1 = Plane("Boeing", "747") #Create a Plane object
+
+for x in (car1, boat1, plane1):
+  print(x.brand)
+  print(x.model)
+  x.move()
+
+#-----------------------------------------------------------
+
+class animals:
+    def __init__(self,species,sound):
+        self.species = species
+        self.sound = sound
+
+    def voice(self):
+        print('Dont Know')
+
+class dog(animals):
+   
+    def voice(self):
+        print('Bark')
+
+class cat(animals):
+
+    def voice(self):
+        print('Meow')
+
+
+x = dog('dog','bark')
+y = cat('cat','meow')
+for i in (x,y):
+    print(i.species)
+    print(i.sound)
+    i.voice()
