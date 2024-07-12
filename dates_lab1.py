baddb7b940a2fbc9454089034064268f3d13b02c
jacobmat8819 <jackmat8819@gmail.com>
2024-07-12 11:05:07 +0530

    Version 1.0

---
+++ dates_lab1.py
@@ -0,0 +1,25 @@
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
+
+
+import datetime
+
+x = datetime.datetime(2024,7,12)
+
+print(x)
+
+import datetime
+
+x = datetime.datetime(2024,7,12)
+
+print(x.strftime("%B"))
+
