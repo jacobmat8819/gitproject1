f58a682dd0afad50aaabe114a47cf4d8661b072e
jacobmat8819 <jackmat8819@gmail.com>
2024-07-25 13:04:07 +0530

    Version 3.0

--- demo_mysql_lab3.py
+++ demo_mysql_lab3.py
@@ -4,36 +4,36 @@ mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="88191988",
-  database="jacobdb",auth_plugin='mysql_native_password'
+  database="northwind",auth_plugin='mysql_native_password'
 )
 
 mycursor = mydb.cursor()
 
 
 #mycursor.execute("CREATE TABLE customers (name VARCHAR(255),address VARCHAR(255),city integer(10))")
-mycursor.execute("SHOW TABLES")
+#mycursor.execute("SHOW TABLES")
 
-for x in mycursor:
-  print(x)
+#for x in mycursor:
+  #print(x)
 
 #Insert values in to the table customers #
 
 #sql = "INSERT INTO customers (name, address,city) VALUES (%s, %s,%s)"
-"""
-val = [
-  ('Peter', 'Lowstreet 4',20),
-  ('Amy', 'Apple st 652',21),
-  ('Hannah', 'Mountain 21',23),
-  ('Michael', 'Valley 345',25),
-  ('Sandy', 'Ocean blvd 2',35),
-  ('Betty', 'Green Grass 1',67),
-  ('Richard', 'Sky st 331',78),
-  ('Susan', 'One way 98',98),
-  ('Vicky', 'Yellow Garden 2',27),
-  ('Ben', 'Park Lane 38',38),
-  ('William', 'Central st 954',54),
-  ('Chuck', 'Main Road 989',89),
-  ('Viola', 'Sideway 1633',63)]"""
+#"""
+#val = [
+  #('Peter', 'Lowstreet 4',20),
+  #('Amy', 'Apple st 652',21),
+  #('Hannah', 'Mountain 21',23),
+  #('Michael', 'Valley 345',25),
+  #('Sandy', 'Ocean blvd 2',35),
+  #('Betty', 'Green Grass 1',67),
+  #('Richard', 'Sky st 331',78),
+  #('Susan', 'One way 98',98),
+  #('Vicky', 'Yellow Garden 2',27),
+  #('Ben', 'Park Lane 38',38),
+  #('William', 'Central st 954',54),
+  #('Chuck', 'Main Road 989',89),
+  #('Viola', 'Sideway 1633',63)]"""
 
 #mycursor.executemany(sql, val)
 
@@ -41,7 +41,7 @@ val = [
 
 #print(mycursor.rowcount, "record inserted.")
 
-mycursor.execute("Select * from customers where name = 'Ben'")
+mycursor.execute("SELECT * FROM Customers ORDER BY Country DESC;")
 
 for y in mycursor:
     print(y)
